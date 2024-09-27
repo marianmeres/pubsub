a747158c05ff5117162e68f1611b6f9b0d969a4e
Marian Meres <marian@meres.sk>
2024-09-27 11:07:37 +0200

    config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,22 @@
-import fs from "node:fs";
+import fs from 'node:fs';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 export default [
 	{
 		input: 'src/index.ts',
-		output: {
-			name: 'pubsub',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			typescript(),
-		]
+		output: { name: 'pubsub', file: pkg.browser, format: 'umd' },
+		plugins: [resolve(), commonjs(), typescript()],
 	},
 	{
 		input: 'src/index.ts',
-		plugins: [ typescript() ],
+		plugins: [typescript()],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+			{ file: pkg.module, format: 'es' },
+		],
+	},
 ];
